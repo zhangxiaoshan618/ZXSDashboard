f886b5c6b619a5a43842afde6f01507d4c37b68b
zhangxiaoshan618 <zhangxiaoshan618@sina.com>
2016-07-28 14:43:54 +0800

    改变黄线和红线的弧度比例

--- ZXSDashboard/ZXSDashboard/ZXSSectionView.swift
+++ ZXSDashboard/ZXSDashboard/ZXSSectionView.swift
@@ -18,7 +18,7 @@ class ZXSSectionView: UIView {
         let centerY = bounds.height * 0.5
         
         // 外圆的半径
-        let radiuOutsid = bounds.width * 0.5 - 1
+        let radiuOutsid = bounds.width * 0.5 - 20
         
         // 外圆和内圆之间的距离 
         let space: CGFloat = 5.0
@@ -26,16 +26,22 @@ class ZXSSectionView: UIView {
         // 内圆的半径
         let radiuInsid = radiuOutsid - space
         
+        // 计算 3/7 π 的时候内圆和外圆上的点的坐标
+        let pointInsidX = centerX - radiuInsid * sin(CGFloat(M_PI) * 3.0 / 7.0)
+        let pointInsidY = centerY + radiuInsid * cos(CGFloat(M_PI) * 3.0 / 7.0)
+        
+        let pointOutsidX = centerX - radiuOutsid * sin(CGFloat(M_PI) * 3.0 / 7.0)
+        let pointOutsidY = centerY + radiuOutsid * cos(CGFloat(M_PI) * 3.0 / 7.0)
         
         // 创建路径对象
         let pathGreen = UIBezierPath()
         
         // 拼接路径
-        pathGreen.moveToPoint(CGPoint(x: centerX , y: 1))
+        pathGreen.moveToPoint(CGPoint(x: centerX , y: centerY - radiuOutsid))
         pathGreen.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuOutsid, startAngle: CGFloat(-M_PI_2), endAngle: CGFloat(M_PI_2), clockwise: true)
         pathGreen.addLineToPoint(CGPoint(x: centerX, y: centerY + radiuInsid))
         pathGreen.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuInsid, startAngle: CGFloat(M_PI_2), endAngle: CGFloat(-M_PI_2), clockwise: false)
-        pathGreen.addLineToPoint(CGPoint(x: centerX, y: 1))
+        pathGreen.addLineToPoint(CGPoint(x: centerX, y: centerY - radiuOutsid))
         
         pathGreen.closePath()
         
@@ -49,9 +55,9 @@ class ZXSSectionView: UIView {
         
         pathYellow.moveToPoint(CGPoint(x: centerX, y: centerY + radiuInsid))
         pathYellow.addLineToPoint(CGPoint(x: centerX, y: centerY + radiuOutsid))
-        pathYellow.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuOutsid, startAngle: CGFloat(M_PI_2), endAngle: CGFloat(M_PI), clockwise: true)
-        pathYellow.addLineToPoint(CGPoint(x: centerX - radiuInsid, y: centerY))
-        pathYellow.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuInsid, startAngle: CGFloat(M_PI), endAngle: CGFloat(M_PI_2), clockwise: false)
+        pathYellow.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuOutsid, startAngle: CGFloat(M_PI_2), endAngle: CGFloat(M_PI * 2 * 5 / 7 - M_PI_2), clockwise: true)
+        pathYellow.addLineToPoint(CGPoint(x: pointOutsidX, y: pointOutsidY))
+        pathYellow.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuInsid, startAngle: CGFloat(M_PI * 2 * 5 / 7 - M_PI_2), endAngle: CGFloat(M_PI_2), clockwise: false)
         
         pathYellow.closePath()
         
@@ -63,17 +69,21 @@ class ZXSSectionView: UIView {
         
         let pathRed = UIBezierPath()
         
-        pathRed.moveToPoint(CGPoint(x: centerX - radiuInsid, y: centerY))
-        pathRed.addLineToPoint(CGPoint(x: centerX - radiuOutsid, y: centerY))
-        pathRed.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuOutsid, startAngle: CGFloat(M_PI), endAngle: CGFloat(3 * M_PI_2), clockwise: true)
+        pathRed.moveToPoint(CGPoint(x: pointInsidX, y: pointInsidY))
+        pathRed.addLineToPoint(CGPoint(x: pointOutsidX, y: pointOutsidY))
+        pathRed.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuOutsid, startAngle: CGFloat(M_PI * 2 * 5 / 7 - M_PI_2), endAngle: CGFloat(3 * M_PI_2), clockwise: true)
         pathRed.addLineToPoint(CGPoint(x: centerX, y: centerY - radiuOutsid))
-        pathRed.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuInsid, startAngle: CGFloat(3 * M_PI_2), endAngle: CGFloat(M_PI), clockwise: false)
+        pathRed.addArcWithCenter(CGPoint(x: centerX, y: centerY), radius: radiuInsid, startAngle: CGFloat(3 * M_PI_2), endAngle: CGFloat(M_PI * 2 * 5 / 7 - M_PI_2), clockwise: false)
         
         pathRed.closePath()
         
         UIColor.redColor().setStroke()
         
         pathRed.stroke()
+        
+//        let pathScale0 = UIBezierPath()
+        
+        
         
     }
 
